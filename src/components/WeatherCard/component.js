de998a08210551296c8ee96be0199079f975d62a
Kami <kaminasri@gmail.com>
2020-10-05 23:28:14 +0300

    Change weather card color

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -7,22 +7,11 @@ import Condition from './Condition';
 import { motion } from 'framer-motion';
 
 const WeatherCard = ({ temp, condition, city, country, icon, getWeather }) => {
-  const minHotTemp = 15;
-  const maxHotTemp = 40;
-  const minColdTemp = 20;
-  const maxColdTemp = -20;
-
-  let highColor = 0;
-  let lowColor = 0;
   let bg = null;
 
   if (temp > minColdTemp) {
-    highColor = (1 - calculateRatio(temp, minHotTemp, maxHotTemp)) * 255;
-    lowColor = highColor - 200;
     bg = `linear-gradient(to bottom, #fdc830, #f37335)`;
   } else {
-    highColor = (1 - calculateRatio(temp, minColdTemp, maxColdTemp)) * 255;
-    lowColor = highColor - 200;
     bg = `linear-gradient(to top, #1a2980, #26d0ce);`;
   }
 
@@ -48,10 +37,6 @@ const WeatherCard = ({ temp, condition, city, country, icon, getWeather }) => {
       </Card>
     </motion.div>
   );
-};
-
-const calculateRatio = (value, min, max) => {
-  return (value - min) / (max - min);
 };
 
 export default WeatherCard;
